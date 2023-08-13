d5261049d51ebafd4115df0ad8f2d21711728572
bebroch <97059293+bebroch@users.noreply.github.com>
2023-08-14 00:31:38 +0300

    new site

---
+++ resources/views/schems/topPanelSchema.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="topPanel topPanelText">
+        <ul>
+            <li>
+                <img id="imageSize" src="{{asset('build/assets/rose-073a4941.png')}}">
+            </li>
+
+            <li>
+                <a href="{{route('home')}}">
+                    <button id="logo">"Алая Роза" артклуб</button>
+                </a>
+            </li>
+
+            @auth('web')
+            <li>
+                <select name="" id="">
+
+                </select>
+                <a class="mini" href="{{route('createCard')}}">
+                    <button>{{Auth::user()->login}}</button>
+                </a>
+                <a href="{{route('createCard')}}">
+                    <div class="circle"></div>
+                </a>
+            </li>
+            @endauth
+
+            @guest('web')
+            <li class="mini">
+                <a href="{{ route('login') }}">
+                    <button>Войти</button>
+                </a>
+            </li>
+            @endguest
+        </ul>
+    </div>
+
+    @yield('content')
+</body>
+</html>
